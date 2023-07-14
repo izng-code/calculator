f6f75216e31e6f9471deb6ae2bfa2edb75e47eb2
izng-code <129785832+izng-code@users.noreply.github.com>
2023-07-14 09:08:36 +0200

    Create operate function

--- calculator.js
+++ calculator.js
@@ -14,3 +14,15 @@ function divide(a, b) {
     return a / b;
 }
 
+let numberOne;
+let numberTwo;
+let operator;
+
+function operate(numberOne,numberTwo,operator) {
+    switch (operator) {
+        case "+": return add(numberOne, numberTwo);
+        case "-": return subtract(numberOne, numberTwo);
+        case "*": return multiply(numberOne, numberTwo);
+        case "/": return divide(numberOne, numberTwo);
+    }   
+}
